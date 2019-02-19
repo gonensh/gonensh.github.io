f41d06e5276c2ada3ded966bad74c3078fd3e723
Gonen Shoham <Gonen_Shoham@vfc.com>
2019-02-19 01:24:13 -0800

    Use a better photo API

--- examples/ng-blog/main.js
+++ examples/ng-blog/main.js
@@ -904,7 +904,7 @@ var PostsEffects = /** @class */ (function () {
                 userId: post.userId,
                 title: post.title,
                 body: post.body,
-                image: 'https://source.unsplash.com/random/230x178?r=' + Math.random(),
+                image: `https://picsum.photos/230/178/?random&randomizer=${Math.random()}`,
                 categories: ['Web', 'Print'].filter(function (category) {
                     return Math.random() > 0.5;
                 }).concat([
